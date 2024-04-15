8266fdc684a1538c9a66a0ae2dd4ccdfc62cfb6c
iamyashsinghh <iamyashsinghh@gmail.com>
2024-04-15 12:36:05 +0530

    ok

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -8,9 +8,20 @@ git pull
 echo "New changes copied to server !"
 
 echo "Installing Dependencies..."
-composer i --yes
+composer install --optimize-autoloader --no-dev
 
-echo "hello"
-pm2 reload app_name/id
+echo "Some Artisan Commands..."
+
+php artisan optimize
+php artisan storage:link
+
+php artisan view:clear
+php artisan route:clear
+php artisan config:clear
+php artisan cache:clear
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
 echo "Deployment Finished!"
