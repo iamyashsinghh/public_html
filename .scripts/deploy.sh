0d121fdd0d7df858e6393b572d4f59eec49c68ad
iamyashsinghh <iamyashsinghh@gmail.com>
2024-04-15 12:53:08 +0530

    ok

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,7 +3,6 @@ set -e
 
 echo "Deployment started..."
 
-# Pull the latest version of the app
 git pull
 echo "New changes copied to server !"
 
@@ -19,9 +18,5 @@ php artisan view:clear
 php artisan route:clear
 php artisan config:clear
 php artisan cache:clear
-
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
 
 echo "Deployment Finished!"
