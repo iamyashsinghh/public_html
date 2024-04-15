04b6d9dc0e8299d3509852ab6a84b335816061a0
iamyashsinghh <iamyashsinghh@gmail.com>
2024-04-15 12:45:11 +0530

    ok

--- routes/api.php
+++ routes/api.php
@@ -37,7 +37,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
 if (!function_exists('getAssigningRm')) {
     function getAssigningRm()
     {
