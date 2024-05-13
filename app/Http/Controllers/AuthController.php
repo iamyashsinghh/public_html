00fcfd92b1e3512b5c913e4f4fe7ab57aebeb6d3
iamyashsinghh <iamyashsinghh@gmail.com>
2024-05-13 14:55:13 +0530

    ok

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -45,7 +45,7 @@ class AuthController extends Controller
         try {
             $verification_code = rand(111111, 999999);
             // $verification_code = 999999;
-            
+
             $agent = new Agent();
             $browser_name = $agent->browser();
             $browser_version = $agent->version($browser_name);
