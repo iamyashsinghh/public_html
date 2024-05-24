b9af9b83b9165e468de5b69cf23ddd228edc9131
iamyashsinghh <iamyashsinghh@gmail.com>
2024-05-24 17:09:57 +0530

    ok

--- resources/views/whatsapp/abdmchat.blade.php
+++ resources/views/whatsapp/abdmchat.blade.php
@@ -129,7 +129,7 @@ $userName = '';
                         <label for="document" class="form-label">Choose Document</label>
                         <input class="form-control" type="file" id="document" name="document" required>
                     </div>
-                        <input class="form-control" type="number" id="phone_inp_id_doc" name="phone_inp_id_doc">
+                        <input class="form-control d-none" type="number" id="phone_inp_id_doc" name="phone_inp_id_doc">
                 </form>
             </div>
             <div class="modal-footer">
@@ -175,7 +175,7 @@ $userName = '';
             var formData = new FormData();
             formData.append('document', documentInput.files[0]);
             formData.append('documentTitle', documentTitle);
-            formData.append('documentTitle', phone_inp_id_doc);
+            formData.append('phone_inp_id_doc', phone_inp_id_doc);
 
 
             $.ajax({
