1c6d15732f4118758dfeca34a21614679bbc991e
Yash Singh <iamyashsinghh@gmail.com>
2024-06-05 16:40:04 +0530

    ok google

--- app/Http/Controllers/GoogleSheetController.php
+++ app/Http/Controllers/GoogleSheetController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Google\Service\Sheets;
 use Illuminate\Http\Request;
 use Google_Client;
+
 class GoogleSheetController extends Controller
 {
     public function getSheetData()
@@ -15,61 +16,22 @@ class GoogleSheetController extends Controller
         $client->setAuthConfig(storage_path('app/google/credentials.json'));
         $client->setAccessType('offline');
 
-        $authUrl = $client->createAuthUrl();
+        $service = new Sheets($client);
 
-        return redirect($authUrl);
+        $spreadsheetId = 'your-spreadsheet-id';
+        $range = 'Sheet1!A1:D10';
 
-        // $client = new Google_Client();
-        // $client->setApplicationName('Google Sheets API Laravel');
-        // $client->setScopes(Sheets::SPREADSHEETS_READONLY);
-        // $client->setAuthConfig(storage_path('app/google/credentials.json'));
-        // $client->setAccessType('offline');
+        try {
+            $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+            $values = $response->getValues();
 
-        // $service = new Sheets($client);
-
-        // $spreadsheetId = 'your-spreadsheet-id';
-        // $range = 'Sheet1!A1:D10';
-
-        // $response = $service->spreadsheets_values->get($spreadsheetId, $range);
-        // $values = $response->getValues();
-
-        // if (empty($values)) {
-        //     return response()->json(['message' => 'No data found.']);
-        // } else {
-        //     return response()->json($values);
-        // }
-    }
-
-    public function handleGoogleCallback(Request $request)
-    {
-        $client = new Google_Client();
-        $client->setApplicationName('Google Sheets API Laravel');
-        $client->setScopes(Sheets::SPREADSHEETS_READONLY);
-        $client->setAuthConfig(storage_path('app/google/credentials.json'));
-        $client->setAccessType('offline');
-
-        if ($request->input('code')) {
-            $client->fetchAccessTokenWithAuthCode($request->input('code'));
-
-            $service = new Sheets($client);
-
-            $spreadsheetId = 'your-spreadsheet-id';
-            $range = 'Sheet1!A1:D10';
-
-            try {
-                $response = $service->spreadsheets_values->get($spreadsheetId, $range);
-                $values = $response->getValues();
-
-                if (empty($values)) {
-                    return response()->json(['message' => 'No data found.']);
-                } else {
-                    return response()->json($values);
-                }
-            } catch (\Exception $e) {
-                return response()->json(['error' => $e->getMessage()]);
+            if (empty($values)) {
+                return response()->json(['message' => 'No data found.']);
+            } else {
+                return response()->json($values);
             }
-        } else {
-            return response()->json(['error' => 'Authorization code not provided.']);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()]);
         }
     }
 }
