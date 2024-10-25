dc1f46cfbe738b852b651b1d4fab06a85bd80a05
Yash Singh <iamyashsinghh@gmail.com>
2024-10-25 18:08:31 +0530

    ok commit

--- resources/views/includes/preloader.blade.php
+++ resources/views/includes/preloader.blade.php
@@ -1,4 +1,4 @@
 <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="" src="{{asset('happy-diwali-diwali.gif')}}" alt="AdminLTELogo" style="width: 30%; height: 50%;">
+    <img class="" src="{{asset('happy-diwali-diwali.png')}}" alt="AdminLTELogo" style="width: 30%; height: 50%;">
     {{-- class="animation__shake" --}}
 </div>
